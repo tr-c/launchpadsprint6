139b82fa58e2f879cd6893b89c67fcb8eba1eb71
tr-c <tc82@rice.edu>
2024-01-20 14:59:45 -0600

    server

--- server/index.js
+++ server/index.js
@@ -25,28 +25,59 @@ app.get("/example", (req, res, next) => {
 });
 
 // TODO ROUTE #1 - Get all shopping items
-
-
+// /getAll - returns all the documents in the database
+app.get("/getAll", (req, res, next) => {
+    res.json(database);
+});
 
 
 // TODO ROUTE #2 - Add a new shopping item
-
-
+// /add - adds a new document to the database
+app.post('/add', (req, res, next) => {
+    database.push({id: database.length + 1, ...req.body})
+    res.json(database);
+})
 
 
 // TODO ROUTE #3 - Remove an existing shopping item
+// /remove - deletes a single document from the database
+// app.post('/remove', (req, res, next) => {
+//     let idx = database.findIndex((item) => item.name == req.body.existingName)
+//     console.log(idx)
+//     database.pop({idx, database[idx].name, database[idx].category})
+//     res.json(database);
+// })
 
 
+app.delete("/remove", (req, res, next) => {
+    let newDatabase = database.filter((item) =>
+        item.name != req.body.name 
+    )
+    database = newDatabase
+    res.json(database)
+})
 
 
 // TODO ROUTE #4 - Update an existing shopping item (harder)
-
+// /update - updates (either the name or category of the item) a single document in the database
+app.put("/update", (req, res, next) => {
+    let idx = database.findIndex((item) => item.name == req.body.existingName)
+    console.log(idx)
+    database[idx].name = req.body.newName
+    database[idx].category = req.body.newCategory
+    res.json(database)
+})
 
 
 
 // TODO ROUTE #5 - Get shopping items that satisfy a condition/filter (harder)
-
-
+// /getByCat - returns all documents that match a certain category in the database
+app.get("/getByCat", (req, res, next) => {
+    let filteredDatabase = database.filter((item) => 
+        item.category == req.query.category
+    )
+    res.json(filteredDatabase)
+})
 
 
 
